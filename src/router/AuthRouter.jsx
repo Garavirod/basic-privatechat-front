13daa1d6be70457753b6f4440a98948b31602f4d
Rodrigo García Ávila <rodrigogarciaavila26@gmail.com>
2020-12-17 16:25:46 -0600

    login and register structurewere coded

--- src/router/AuthRouter.jsx
+++ src/router/AuthRouter.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import { Redirect, Route, Switch } from 'react-router-dom';
-import { LoginView } from '../views/LoginView';
-import { RegisterView } from '../views/RegisterView';
+import React from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
+import { LoginView } from "../views/LoginView";
+import { RegisterView } from "../views/RegisterView";
+// css
+import "../css/login-register.css";
 
 export const AuthRouter = () => {
-    return ( 
-        <Switch>
-            <Route exact path="/auth/login" component={ LoginView }/>
-            <Route exact path="/auth/register" component={ RegisterView }/>
-            <Redirect to="/auth/login"/>
-        </Switch>
-    )
-}
+  return (
+    <div className="limiter">
+      <div className="container-login100">
+        <div className="wrap-login100 p-t-50 p-b-90">
+            <Switch>
+                <Route exact path="/auth/login" component={ LoginView }/>
+                <Route exact path="/auth/register" component={ RegisterView }/>
+                <Redirect to="/auth/login"/>
+            </Switch>
+        </div>
+      </div>
+    </div>
+  );
+};
